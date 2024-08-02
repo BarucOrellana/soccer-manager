3143dd9d0245674a645f3de85ee0c155391a7541
baruc <barucorellana@gmail.com>
2024-08-01 18:37:11 -0600

    modified (game model): add date}

--- Soccer-Manager/src/main/java/org/app/model/GameModel.java
+++ Soccer-Manager/src/main/java/org/app/model/GameModel.java
@@ -2,11 +2,14 @@ package org.app.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class GameModel {
     private int id;
     private int visitTeam;
     private int localTeam;
+    private LocalDate date;
     private String tournament;
     private int goalsVisitTeam;
     private int goalsLocalTeam;
