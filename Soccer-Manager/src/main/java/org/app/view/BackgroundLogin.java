a7abb036bb9957c6b69a9ac8bdd3f5004912f1a4
baruc <barucorellana@gmail.com>
2024-11-04 23:25:42 -0600

    add uploaded image

--- Soccer-Manager/src/main/java/org/app/view/BackgroundLogin.java
+++ Soccer-Manager/src/main/java/org/app/view/BackgroundLogin.java
@@ -1,2 +1,22 @@
-package org.app.view;public class BackgroundLogin {
+package org.app.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+public class BackgroundLogin extends JPanel {
+    private final Image image;
+
+    public BackgroundLogin() {
+        image = new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/Diseño_sin_título_(16).png"))).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (image != null) {
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        }
+        setOpaque(false);
+    }
 }
